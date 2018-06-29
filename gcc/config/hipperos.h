096382a1900141856bc14772fafa827f82df6280
Laurent Deru <laurent.deru@cetic.be>
2018-06-29 15:18:54 +0200

    Add huser hipperos specific option and clean up library specification

--- gcc/config/hipperos.h
+++ gcc/config/hipperos.h
@@ -4,8 +4,20 @@
 #undef TARGET_HIPPEROS
 #define TARGET_HIPPEROS 1
 
+#undef STARTFILE_SPEC
+#define STARTFILE_SPEC ""
+
+#undef ENDFILE_SPEC
+#define ENDFILE_SPEC   ""
+
+#define STD_LIB_SPEC ""
+
+#undef LIBGCC_SPEC
+#define LIBGCC_SPEC ""
+
 #undef LIB_SPEC
-#define LIB_SPEC ""
+#define LIB_SPEC "%{!huser: " STD_LIB_SPEC "} " \
+"%{!nostdlib: %{huser: --start-group -lapi -lcompiler_rt -lc --end-group}}"
 
 #undef TARGET_OS_CPP_BUILTINS
 #define TARGET_OS_CPP_BUILTINS() \
